0cdc9c16ab4c64e7578c802bca97267f6ffff899
Maksim Zholudev <maximzms@gmail.com>
2022-12-17 00:42:58 +0300

    Improve answer

--- 01_cyclomatic_complexity/example.py
+++ 01_cyclomatic_complexity/example.py
@@ -3,14 +3,15 @@ def foo(a, b, c, d):
     even_names = []
     for name, value in zip(["A", "B", "C", "D"], [a, b, c, d]):
         (even_names if value % 2 == 0 else odd_names).append(name)
-    answer_generators = [
-        lambda odd, even: 'Все числа четные',
-        lambda odd, even: f'Все числа четные, кроме числа {odd[0]}',
-        lambda odd, even: f'Числа {even[0]} и {even[1]} четные, а {odd[0]} и {odd[1]} нет',
-        lambda odd, even: f'Все числа нечетные, кроме числа {even_names[0]}',
-        lambda odd, even: 'Все числа нечетные',
-    ]
-    return answer_generators[len(odd_names)](odd_names, even_names)
+    return _answer_generators[len(odd_names)](odd_names, even_names)
+
+_answer_generators = [
+    lambda odd, even: 'Все числа четные',
+    lambda odd, even: f'Все числа четные, кроме числа {odd[0]}',
+    lambda odd, even: f'Числа {even[0]} и {even[1]} четные, а {odd[0]} и {odd[1]} нет',
+    lambda odd, even: f'Все числа нечетные, кроме числа {even[0]}',
+    lambda odd, even: 'Все числа нечетные',
+]
 
 assert(foo(2, 4, 6, 8) == 'Все числа четные')
 assert(foo(3, 4, 6, 8) == 'Все числа четные, кроме числа A')
